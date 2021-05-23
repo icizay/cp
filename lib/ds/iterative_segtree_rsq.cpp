fcfa6165c7eae6bb5f88e23056b5e3de4603d1a5
bedirhan <bedirhanyazicix@gmail.com>
2021-05-23 12:31:17 +0300

    Update iterative_segtree_rsq.cpp

--- lib/ds/iterative_segtree_rsq.cpp
+++ lib/ds/iterative_segtree_rsq.cpp
@@ -1,22 +1,26 @@
-// didn't test yet
+struct segtree{
+	int N = 1e5 + 5; ll KEK = 0;
+	vector<ll> st;
+	segtree(int _N, ll _KEK) : N(_N), KEK(_KEK) {
+		st.resize(4 * N);
+	}
 
-const int N = 1e5;  // limit for array size
-int n;  // array size
-int t[2 * N]; // 3 or 4(?)
+	ll f(ll x, ll y){ return min(x, y);}
 
-void build() {  // build the tree
-    for(int i = n - 1; i > 0; --i) t[i] = t[i << 1] + t[i << 1 | 1];
-}
+	void build() {
+		for(int i = N - 1; i > 0; --i) st[i] = f(st[i << 1], st[i << 1 | 1]);
+	}
 
-void modify(int p, int value) {  // set value at position p
-    for(t[p += n] = value; p > 1; p >>= 1) t[p >> 1] = t[p] + t[p ^ 1];
-}
+	void upd(int p, ll val) { 
+		for(st[p += N] = val; p > 1; p >>= 1) st[p >> 1] = f(st[p], st[p ^ 1]);
+	}
 
-int query(int l, int r){  // sum on interval [l, r)
-    int res = 0;
-    for(l += n, r += n; l < r; l >>= 1, r >>= 1){
-	if (l & 1) res += t[l++];
-	if (r & 1) res += t[--r];
-    }
-    return res;
-}
+	ll qry(int l, int r){ 
+		ll res = KEK;
+		for(l += N, r += N; l < r; l >>= 1, r >>= 1){
+			if (l & 1) res = f(res, st[l++]);
+			if (r & 1) res = f(res, st[--r]);
+		}
+		return res;
+	}
+};
